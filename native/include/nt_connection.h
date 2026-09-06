8b927bfae433789e5e03e6cd28b617f78430ea7c
einekuhmachtmuh <c44986054@gmail.com>
2026-09-06 18:32:54 +0800

    Keep connection API independent of epoll

--- native/include/nt_connection.h
+++ native/include/nt_connection.h
@@ -2,7 +2,6 @@
 #define NT_CONNECTION_H
 
 #include <stddef.h>
-#include <stdint.h>
 #include <sys/types.h>
 
 #ifdef __cplusplus
@@ -18,7 +17,6 @@ typedef enum nt_connection_state {
 } nt_connection_state_t;
 
 int nt_connection_create(nt_connection_t **connection, int fd);
-int nt_connection_set_events(nt_connection_t *connection, uint32_t events);
 int nt_connection_get_fd(const nt_connection_t *connection);
 nt_connection_state_t nt_connection_get_state(const nt_connection_t *connection);
 int nt_connection_read(nt_connection_t *connection, void *buffer, size_t length, ssize_t *result);
@@ -27,6 +25,7 @@ void nt_connection_close(nt_connection_t *connection);
 void nt_connection_destroy(nt_connection_t *connection);
 
 #ifdef __cplusplus
-}
+extern "C" {
+#endif
 
 #endif
