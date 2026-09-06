d2930461069d54f91dec345cf0a695fab865cc5f
einekuhmachtmuh <c44986054@gmail.com>
2026-09-06 18:37:27 +0800

    Associate native connections with owning runtime

--- native/include/nt_connection.h
+++ native/include/nt_connection.h
@@ -9,6 +9,7 @@ extern "C" {
 #endif
 
 typedef struct nt_connection nt_connection_t;
+typedef struct nt_runtime nt_runtime_t;
 
 typedef enum nt_connection_state {
 	NT_CONNECTION_ACTIVE = 0,
@@ -16,8 +17,9 @@ typedef enum nt_connection_state {
 	NT_CONNECTION_CLOSED = 2
 } nt_connection_state_t;
 
-int nt_connection_create(nt_connection_t **connection, int fd);
+int nt_connection_create(nt_connection_t **connection, nt_runtime_t *runtime, int fd);
 int nt_connection_get_fd(const nt_connection_t *connection);
+nt_runtime_t *nt_connection_get_runtime(const nt_connection_t *connection);
 nt_connection_state_t nt_connection_get_state(const nt_connection_t *connection);
 int nt_connection_read(nt_connection_t *connection, void *buffer, size_t length, ssize_t *result);
 int nt_connection_write(nt_connection_t *connection, const void *buffer, size_t length, ssize_t *result);
@@ -26,7 +28,6 @@ void nt_connection_destroy(nt_connection_t *connection);
 
 #ifdef __cplusplus
 }
-
 #endif
 
 #endif
