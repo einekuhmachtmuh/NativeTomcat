500db95cf480608cc8a6173e964062601ca0078a
einekuhmachtmuh <c44986054@gmail.com>
2026-09-06 19:19:13 +0800

    Add embedded JVM lifecycle interface

---
+++ native/include/nt_jvm.h
@@ -0,0 +1,27 @@
+#ifndef NT_JVM_H
+#define NT_JVM_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct nt_jvm nt_jvm_t;
+
+typedef struct nt_jvm_config {
+	const char *java_home;
+	const char *class_path;
+	const char *bootstrap_class;
+} nt_jvm_config_t;
+
+int nt_jvm_start(nt_jvm_t **jvm, const nt_jvm_config_t *config);
+int nt_jvm_stop(nt_jvm_t *jvm);
+void nt_jvm_destroy(nt_jvm_t *jvm);
+bool nt_jvm_is_ready(const nt_jvm_t *jvm);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
