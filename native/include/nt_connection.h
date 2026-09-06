7ba850d6e8554a1f29f9e7a04f46c404849d76c7
einekuhmachtmuh <c44986054@gmail.com>
2026-09-06 18:39:44 +0800

    Define explicit native connection I/O result contract

--- native/include/nt_connection.h
+++ native/include/nt_connection.h
@@ -18,14 +18,21 @@ typedef enum nt_connection_state {
 	NT_CONNECTION_CLOSED = 2
 } nt_connection_state_t;
 
+typedef enum nt_connection_io_result {
+	NT_CONNECTION_IO_OK = 0,
+	NT_CONNECTION_IO_WOULD_BLOCK = 1,
+	NT_CONNECTION_IO_EOF = 2,
+	NT_CONNECTION_IO_ERROR = -1
+} nt_connection_io_result_t;
+
 int nt_connection_create(nt_connection_t **connection, nt_runtime_t *runtime, int fd);
 int nt_connection_get_fd(const nt_connection_t *connection);
 nt_runtime_t *nt_connection_get_runtime(const nt_connection_t *connection);
 nt_connection_state_t nt_connection_get_state(const nt_connection_t *connection);
 bool nt_connection_peer_read_closed(const nt_connection_t *connection);
 void nt_connection_mark_peer_read_closed(nt_connection_t *connection);
-int nt_connection_read(nt_connection_t *connection, void *buffer, size_t length, ssize_t *result);
-int nt_connection_write(nt_connection_t *connection, const void *buffer, size_t length, ssize_t *result);
+nt_connection_io_result_t nt_connection_read(nt_connection_t *connection, void *buffer, size_t length, ssize_t *result);
+nt_connection_io_result_t nt_connection_write(nt_connection_t *connection, const void *buffer, size_t length, ssize_t *result);
 void nt_connection_close(nt_connection_t *connection);
 void nt_connection_destroy(nt_connection_t *connection);
 
