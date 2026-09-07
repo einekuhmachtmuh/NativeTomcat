2e0e10fbb211a1ff8e63dad0eb382e0c0108e28c
einekuhmachtmuh <c44986054@gmail.com>
2026-09-07 15:16:48 +0800

    native: represent read/write interest as a mask

--- native/include/nt_runtime.h
+++ native/include/nt_runtime.h
@@ -19,6 +19,11 @@ typedef enum nt_runtime_event {
 	NT_RUNTIME_EVENT_ERROR = 1u << 3
 } nt_runtime_event_t;
 
+typedef enum nt_runtime_interest {
+	NT_RUNTIME_INTEREST_READ = 1u << 0,
+	NT_RUNTIME_INTEREST_WRITE = 1u << 1
+} nt_runtime_interest_t;
+
 typedef void (*nt_runtime_connection_handler_t)(
 		nt_runtime_t *runtime, nt_connection_t *connection, unsigned events, void *user_data);
 
@@ -34,11 +39,11 @@ typedef struct nt_runtime_config {
 int nt_runtime_init(nt_runtime_t **runtime, const nt_runtime_config_t *config);
 int nt_runtime_get_port(const nt_runtime_t *runtime);
 nt_connection_t *nt_runtime_find_connection(nt_runtime_t *runtime, uint64_t handle);
-int nt_runtime_request_rearm(nt_runtime_t *runtime, uint64_t handle, bool want_write);
+int nt_runtime_request_rearm(nt_runtime_t *runtime, uint64_t handle, unsigned interest_mask);
 int nt_runtime_request_close(nt_runtime_t *runtime, uint64_t handle);
 int nt_runtime_run(nt_runtime_t *runtime);
 void nt_runtime_stop(nt_runtime_t *runtime);
-int nt_runtime_rearm_connection(nt_runtime_t *runtime, nt_connection_t *connection, bool want_write);
+int nt_runtime_rearm_connection(nt_runtime_t *runtime, nt_connection_t *connection, unsigned interest_mask);
 void nt_runtime_destroy(nt_runtime_t *runtime);
 
 #ifdef __cplusplus
