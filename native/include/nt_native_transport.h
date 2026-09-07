252e5b6452487228e2247402d91502a33d207856
einekuhmachtmuh <c44986054@gmail.com>
2026-09-07 13:08:05 +0800

    native: add transport runtime binding interface

---
+++ native/include/nt_native_transport.h
@@ -0,0 +1,16 @@
+#ifndef NT_NATIVE_TRANSPORT_H
+#define NT_NATIVE_TRANSPORT_H
+
+#include "nt_runtime.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int nt_native_transport_bind_runtime(nt_runtime_t *runtime);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
