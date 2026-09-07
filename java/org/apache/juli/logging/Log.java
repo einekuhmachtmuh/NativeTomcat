0e16e09056a2eb607d90ca262a6b643712b4b8e4
einekuhmachtmuh <c44986054@gmail.com>
2026-09-07 08:32:06 +0800

    Correct Log.java to pinned Tomcat source

--- java/org/apache/juli/logging/Log.java
+++ java/org/apache/juli/logging/Log.java
@@ -256,7 +256,7 @@ public interface Log {
      * Log an error with fatal log level.
      * </p>
      *
-     * @param message log this cause
+     * @param message log this message
      * @param t       log this cause
      */
     void fatal(Object message, Throwable t);
